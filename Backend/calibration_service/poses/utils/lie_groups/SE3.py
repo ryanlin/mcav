ab69657e3e6a6a888cf980bcd93f5cf24f0d0849
tman0004 <tman0004@gmail.com>
2021-04-06 18:34:25 -0700

    fixing my bug with backend code

--- Backend/calibration_service/poses/utils/lie_groups/SE3.py
+++ Backend/calibration_service/poses/utils/lie_groups/SE3.py
@@ -1,32 +1,44 @@
 import numpy as np
-from.skew_symmetric import skew_symmetric
 
 
-def exp(omega_u: np.ndarray):
-    """Returns SE(3) as a matrix on a manifold
+def inverse(T: np.ndarray):
+    assert T.shape == (4, 4)
 
-    omega_u: (angular, linear)
-    """
-    assert omega_u.shape == (6,)
+    R = T[0:3, 0:3].copy()
+    t = T[0:3, 3].copy()
+    return np.block([[R.T, np.atleast_2d(-R.T @ t).T], [0.0, 0.0, 0.0, 1.0]])
 
-    omega = omega_u[0:3]
-    u = omega_u[3:6]
 
-    theta = np.sqrt(omega.T @ omega)
+def log(T: np.ndarray):
+    """Returns se(3) as a vector of six elements (angular, linear) on a tangent space"""
+    assert T.shape == (4, 4)
+
+    R = T[0:3, 0:3].copy()
+    t = T[0:3, 3].copy()
+
+    # Numpy arccos expects input to be between -1.0 and 1.0
+    theta = np.arccos(np.clip((np.trace(R) - 1.0) / 2.0, -1.0, 1.0))
 
     if theta < 0.001:
+        D = 0.5 + (theta**2.0 / 12.0) + ((7.0 * theta**4.0) / 720.0)
         A = 1.0 - (theta**2.0) / 6.0 + (theta**4.0) / 120.0
         B = 0.5 - (theta**2.0) / 24.0 + (theta**4.0) / 720.0
-        C = 1.0 / 6.0 - theta**2.0 / 120.0 + (theta**4.0) / 5040.0
     else:
+        D = theta / (2.0 * np.sin(theta))
         A = np.sin(theta) / theta
-        B = (1.0 - np.cos(theta)) / theta ** 2.0
-        C = (1 - A) / theta**2.0
+        B = (1.0 - np.cos(theta)) / theta**2.0
 
-    omega_x = skew_symmetric(omega)
+    omega_x = D * (R - R.T)
 
-    R = np.eye(3) + A * omega_x + B * omega_x @ omega_x
-    V = np.eye(3) + B * omega_x + C * omega_x @ omega_x
+    if theta < 0.001:
+        C = 1.0 / 6.0 - theta**2.0 / 120.0 + (theta**4.0) / 5040.0
+        V_inv = np.linalg.inv(np.eye(3) + B * omega_x +
+                              C * (omega_x @ omega_x))
+    else:
+        V_inv = np.eye(3) - (0.5 * omega_x) + ((1.0 / theta**2.0)
+                                               * (1.0 - A / (2.0 * B))) * (omega_x @ omega_x)
+    u = V_inv @ t
 
-    return np.block([[R, V @ np.expand_dims(u, axis=1)],
-                     [np.zeros(3), 1.0]])
+    omega = np.array([omega_x[2, 1], -omega_x[2, 0], omega_x[1, 0]])
+
+    return np.block([omega, u])
