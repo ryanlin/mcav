b37d38133c10c3b13e2484900771471c9f0b5ae7
Eric Duong <eduong@nevada.unr.edu>
2021-04-07 11:47:58 -0700

    Revert changes to back-end

--- Backend/calibration_service/poses/utils/lie_groups/so3.py
+++ Backend/calibration_service/poses/utils/lie_groups/so3.py
@@ -1,21 +1,20 @@
 import numpy as np
+from.skew_symmetric import skew_symmetric
 
 
-def inverse(R: np.ndarray):
-    assert R.shape == (3, 3)
-    return R.T.copy()
+def exp(omega: np.ndarray):
+    """Returns SO(3) as a rotation matrix on a manifold"""
+    assert omega.shape == (3,)
 
-
-def log(R: np.ndarray):
-    """Returns so(3) as a vector of three elements on a tangent space"""
-    assert R.shape == (3, 3)
-
-    theta = np.arccos(np.clip((np.trace(R) - 1.0) / 2.0, -1.0, 1.0))
+    theta = np.sqrt(omega.T @ omega)
 
     if theta < 0.001:
-        A = 0.5 + (theta ** 2.0) / 12.0 + (7.0 * theta ** 4.0) / 720.0
+        A = 1.0 - (theta ** 2.0) / 6.0 + (theta ** 4.0) / 120.0
+        B = 0.5 - (theta ** 2.0) / 24.0 + (theta ** 4.0) / 720.0
     else:
-        A = theta / (2.0 * np.sin(theta))
+        A = np.sin(theta) / theta
+        B = (1.0 - np.cos(theta)) / theta ** 2.0
 
-    omega_x = A * (R - R.T)
-    return np.array([omega_x[2, 1], -omega_x[2, 0], omega_x[1, 0]])
+    omega_x = skew_symmetric(omega)
+
+    return np.eye(3) + A * omega_x + B * omega_x @ omega_x
