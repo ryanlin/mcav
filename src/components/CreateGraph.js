0ff9dcbaa784cd6d4357809e4bb886e09694980c
Taiga Miyano <tman0004@gmail.com>
2021-04-07 17:26:29 -0700

    Calibration merge (#27)
    * added calibration btn with backend
    
    * Added save graph button
    
    Co-authored-by: nrose171 <nrose171@gmail.com>

--- src/components/CreateGraph.js
+++ src/components/CreateGraph.js
@@ -25,10 +25,16 @@ const CreateGraph = () => {
   var [connectors, setConnectors] = React.useState(INITIAL_STATE);
   var [fromShapeId, setFromShapeId] = React.useState(null);
   var [displayID, setDisplayID] = React.useState("N/A");
+  var [saveFile, setSaveFile] = useState("null");
 
   // Handlers
   const handleFileUpload = e => {
     setFileState(e.target.files[0]);
+    console.log(e.target.files[0].path);
+    circles.forEach( circle => {
+      circle.rosbagPath = e.target.files[0].path;
+    });
+    console.log(circles);
   };
 
   const onClickCalibrate = () => {
@@ -38,8 +44,13 @@ const CreateGraph = () => {
       nodes: circles,
       edges: connectors
     };
-    console.log(fullGraph);
-    // api.calibration("calibration", fullGraph);
+    api.receive("calibration", (res) => {
+      console.log("calibration recieved");
+      console.log(res);
+    }, []);
+    //console.log(fullGraph);
+    api.calibration("calibration", fullGraph);
+    setSaveFile(fullGraph);
   }
 
   const selectTopic = (e) => {
@@ -47,13 +58,20 @@ const CreateGraph = () => {
   }
 
   // API Receivers:
-  api.receive("bagfile", (res) => {
+  /*api.receive("bagfile", (res) => {
     console.log("bagfile recieved");
     topicList = JSON.parse(JSON.stringify(res));
     console.log(topicList);
 
     setBagTopics(topicList);
+  }, []);*/
+
+/*
+  api.receive("calibration", (res) => {
+    console.log("calibration recieved");
+    console.log(res);
   }, []);
+*/
 
   return (
     <div>
@@ -225,6 +243,7 @@ const CreateGraph = () => {
                   x: e.target.x(),
                   y: e.target.y(),
                   id: circles.length,
+                  type: "pose",
                   sensorType: "lidar",
                   type: "pose",
                   topic: "null"
@@ -262,7 +281,7 @@ const CreateGraph = () => {
 
           <Text
             x={30}
-            y={400}
+            y={395}
             fontSize={20}
             text={"Sensor Type: " + displayID}
             fill="black"
@@ -313,8 +332,8 @@ const CreateGraph = () => {
                   else if( !checkEdges(fromShapeId, eachCircle, connectors) ) {
                     const newConnector = {
                       id: connectors.length,
-                      sourceNodeId: fromShapeId,
-                      targetNodeId: eachCircle.id
+                      sourceNodeID: fromShapeId,
+                      targetNodeID: eachCircle.id
                     };
                     setConnectors(connectors.concat([newConnector]));
                     setFromShapeId(null);
@@ -346,13 +365,13 @@ const CreateGraph = () => {
           ))}
 
           {connectors.map(con => {
-            const sourceNodeId = circles.find(s => s.id === con.sourceNodeId);
-            const targetNodeId = circles.find(s => s.id === con.targetNodeId);
+            const sourceNodeID = circles.find(s => s.id === con.sourceNodeID);
+            const targetNodeID = circles.find(s => s.id === con.targetNodeID);
 
             return (
               <Arrow
                 key={con.id}
-                points={[sourceNodeId.x, sourceNodeId.y, targetNodeId.x, targetNodeId.y]}
+                points={[sourceNodeID.x, sourceNodeID.y, targetNodeID.x, targetNodeID.y]}
                 stroke="black"
               />
 
@@ -375,6 +394,14 @@ const CreateGraph = () => {
         onClick={() => {
           console.log(fileState.path);
           api.rosbag("rosbag", fileState.path);
+          api.receive("bagfile", (res) => {
+            console.log("bagfile recieved");
+            topicList = JSON.parse(JSON.stringify(res));
+            console.log(topicList);
+
+            setBagTopics(topicList);
+
+          }, []);
         }}
         style={{
           position: 'absolute',
@@ -401,12 +428,31 @@ const CreateGraph = () => {
           borderRadius: "12px",
           color: "white",
           position: 'absolute',
-          top: 570,
-          left: 65
+          top: 530,
+          left: 63
         }}
         >
           Calibrate!
-      </button>      
+      </button>
+
+      <button
+        onClick={() => {
+          const fileData = JSON.stringify(saveFile, null, 4);
+          const blob = new Blob([fileData], {type: "text/plain"});
+          const url = URL.createObjectURL(blob);
+          const link = document.createElement('a');
+          link.download = 'filename.json';
+          link.href = url;
+          link.click();
+        }}
+        style={{
+          position: 'absolute',
+          top: 590,
+          left: 79
+        }}
+        >
+          Save Graph
+      </button>
     </div>
   );
 };
@@ -425,15 +471,17 @@ api.receive("clear", (res) => {
 
 // }, []);
 
+/*
 api.receive("calibration", (res) => {
   console.log("calibration recieved");
   console.log(res);
 }, []);
+*/
 
 function checkEdges(fromShapeId, eachCircle, edges) {
   for(var i = 0; i < edges.length; i++ ){
-    if( (edges[i].sourceNodeId == fromShapeId || edges[i].sourceNodeId == eachCircle.id) &&
-        (edges[i].targetNodeId == fromShapeId || edges[i].targetNodeId == eachCircle.id) ) {
+    if( (edges[i].sourceNodeID == fromShapeId || edges[i].sourceNodeID == eachCircle.id) &&
+        (edges[i].targetNodeID == fromShapeId || edges[i].targetNodeID == eachCircle.id) ) {
       console.log(true);
       return true;
     }
