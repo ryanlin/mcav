b84d18ac23ba41b47904f489b1f3b820821c7ade
ryanlin <ruilinl@nevada.unr.edu>
2021-04-25 12:42:23 -0700

    Removed old/unused files

--- Frontend/src/components/panels/index.js
+++ Frontend/src/components/panels/index.js
@@ -1,3 +1,1 @@
 export { CalibrationPanels, onClickCalibrate } from './CalibrationPanels';
-export { CanvasButton, onMouseOverButton, onMouseOutButton, clearCanvas, clearEdges } from './CanvasButton';
-export { NodeTool, addGPSCircle, addLidarCircle, onMouseOverNodeTool, onMouseOutNodeTool} from './NodeTool'
